3bcecf97efdace9fbb262d609ad68822c06d163a
jang0820 <jang0820@gmail.com>
2017-11-05 13:43:40 +0800

    新增修課學生

--- tea-class.php
+++ tea-class.php
@@ -86,6 +86,25 @@
     echo "<input type='submit' value='上傳作業單元名稱'></form>";//上傳作業單元名稱
     echo "<a href='tea-class-exe.php?cls_id=$cls_id'>查詢目前學生作業繳交情形</a>";//依照學號列出所有作業在同一列
     echo "</div>";
+
+    $sql = "SELECT acc,cls_id FROM stu_class WHERE cls_id='".$cls_id."'";//查詢所有的修課學生帳號
+    $r1=mysqli_query($db,$sql);
+    echo "<div class='exe'>";
+    if (mysqli_num_rows($r1) > 0){//新增至少一個作業名稱
+      echo "<table border=1><tr><td>帳號</td><td>課程編號</td></tr>";
+      $num=mysqli_num_rows($r1);
+      for($i=0;$i<$num;$i++){
+        $row=mysqli_fetch_row($r1);
+        echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
+      }
+      echo "</table>";
+    }
+    echo "<form class='form' action='tea-stu-class-action.php' enctype='multipart/form-data' method='POST'>";//新增作業
+    echo "<a href ='csv/stu-class.csv'>下載學生選課清單CSV範例檔</a>";//下載CSV範例檔
+    echo "<div> <input type='file' name='class'></div>";//選擇檔案
+    echo "<input type='hidden' name='cls_id' value='".$cls_id."'>";//使用POST傳送cls_id給接收網頁
+    echo "<input type='submit' value='上傳學生選課清單'></form>";//上傳學生選課清單
+    echo "</div>";
     mysqli_close($db); 
   ?>
 </body>
---
+++ tea-stu-class-action.php
@@ -0,0 +1,58 @@
+<?php //上傳作業action接收程式
+//php預設執行時間限制為30秒，修改php.ini的max_execution_time=0，表示沒有限制
+  session_start();
+  if (!isset($_SESSION['prio'])) header("Location: login.php");
+  if ($_SESSION['prio'] != 1) header("Location: login.php");
+  if ($_POST['cls_id']) $cls_id=$_POST['cls_id']; else header("Location: login.php");
+  if (isset($_SESSION["acc"]) ) { 
+    $acc = $_SESSION["acc"];
+    $db = mysqli_connect("localhost", "tea", "cococola");
+    mysqli_select_db($db, "tea");
+    mysqli_query($db,"SET CHARACTER SET UTF8");   
+  } else {
+    header("Location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+<title>教師頁面</title>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+  if (isset($_FILES['class'])) {
+    echo $_FILES['class']['name']."<br>";
+    echo $_FILES['class']['tmp_name']."<br>";
+    echo $_FILES['class']['size']."<br>";
+    echo $_FILES['class']['type']."<br>";
+    $sql = "DELETE FROM stu_class WHERE cls_id='".$cls_id."'";//刪除$cls_id的所有測驗
+    $r1=mysqli_query($db,$sql);
+    if (($handle = fopen($_FILES['class']['tmp_name'], "r")) !== FALSE) {
+      $row=0;
+      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $row++;
+        if ($row!=1){ //忽略標題列
+          $sql="INSERT INTO stu_class VALUES('$data[0]','$cls_id')";//新增選課學生
+          $r1=mysqli_query($db,$sql);
+        }
+      }
+      fclose($handle);
+      $sql = "SELECT acc,cls_id FROM stu_class WHERE cls_id='".$cls_id."'";//查詢所有的修課學生帳號
+      $r1=mysqli_query($db,$sql);
+      echo "<div class='exe'>";
+      if (mysqli_num_rows($r1) > 0){//新增至少一個作業名稱
+        echo "<table border=1><tr><td>帳號</td><td>課程編號</td></tr>";
+        $num=mysqli_num_rows($r1);
+        for($i=0;$i<$num;$i++){
+          $row=mysqli_fetch_row($r1);
+          echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td></tr>";
+        }
+        echo "</table>";
+      }
+      echo "新增選課學生成功";
+    }
+  }
+?>
+</body>
+</html>
